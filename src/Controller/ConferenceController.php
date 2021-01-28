fc6ec70c795b6029a066dc811f2acfbc2b301b1d
Simon Bataille <bataille.simon119@gmail.com>
2021-01-28 11:03:17 -1000

    Add Cache to application

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -7,6 +7,7 @@ use App\Entity\Conference;
 use App\Form\CommentFormType;
 use App\Message\CommentMessage;
 use App\Repository\CommentRepository;
+use App\Repository\ConferenceRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
@@ -35,7 +36,21 @@ class ConferenceController extends AbstractController
      */
     public function index(): Response
     {
-        return new Response($this->twig->render('conference/index.html.twig'));
+        $response =  new Response($this->twig->render('conference/index.html.twig'));
+        $response->setSharedMaxAge(3600);
+        return $response;
+    }
+
+    /**
+     * @Route("/conference_header", name="conference_header")
+     */
+    public function conferenceHeader(ConferenceRepository $conferenceRepository): Response
+    {
+        $response = new Response($this->twig->render('conference/header.html.twig', [
+            'conferences' => $conferenceRepository->findAll(),
+        ]));
+        $response->setSharedMaxAge(3600);
+        return $response;
     }
 
     /**
